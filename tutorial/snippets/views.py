440f2958ef268e381a4e09878c2c66caeb5025ff
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2018-08-29 01:04:46 +0200

    Tutorial - completed step 3 - using generic class-based views.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from tutorial.snippets.models import Snippet
 from tutorial.snippets.serializers import SnippetSerializer
@@ -7,45 +6,23 @@ from tutorial.snippets.serializers import SnippetSerializer
 
 # region Snippet - List & Create
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
-
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
 # endregion
 
 # region Snippet - Details & Edit & Delete
 
-class SnippetDetails(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class SnippetDetails(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
-
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwags):
-        return self.retrieve(request, *args, **kwags)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 # endregion
